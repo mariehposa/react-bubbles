9cc7b059deb77f76955e6a69063f5ca82231e79a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-18 09:06:34 +0100

    Build a `PrivateRoute` component and use it to protect a route that renders the `BubblesPage` component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Login from "./components/Login";
 import "./styles.scss";
+import { withRouter, Redirect } from 'react-router-dom';
+import BubblePage from './components/BubblePage';
 
-function App() {
+export function App() {
   return (
     <Router>
       <div className="App">
@@ -13,9 +14,19 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Route path="/bubblePage" render={props => privateRoute(BubblePage, props)} />
       </div>
     </Router>
   );
+}
+
+function privateRoute (Component, props) {
+  if (localStorage.getItem('token')) {
+    return <Component {...props}/>
+  }
+  else {
+    return <Redirect to="/"/>
+  }
 }
 
 export default App;
